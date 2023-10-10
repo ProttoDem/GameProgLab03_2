a69c98446341360baec9c1b2570e64823ec7772b
ProttoDem <79105660+ProttoDem@users.noreply.github.com>
2023-10-10 20:01:22 +0300

    Added new scene and scripts

---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets
+{
+    public class PlayerCamera : MonoBehaviour
+    {
+        public float mouseSensitivity = 100f;
+
+        public Transform playerBody;
+
+        float xRotation = 0f;
+
+        public void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        public void Update()
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * mouseSensitivity;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * mouseSensitivity;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
